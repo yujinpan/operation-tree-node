8f3dd0f3fef78e0e2aa1e23e6fa8ae227b453d48
yujinpan <1192878390@qq.com>
2019-11-02 14:50:01 +0800

    feat: export the new method `treeFind`

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,9 @@
 export { default as treeEach } from './treeEach';
-export { default as treeEachParent } from './treeEachParent';
+export { default as treeFind } from './treeFind';
 export { default as treeMap } from './treeMap';
 export { default as treeFilter } from './treeFilter';
 export { default as treeToFlatArray } from './treeToFlatArray';
 export { default as treeMerge } from './treeMerge';
+export { default as treeEachParent } from './treeEachParent';
 export { default as treeSort } from './treeSort';
 export { default as treeCheck } from './treeCheck';
